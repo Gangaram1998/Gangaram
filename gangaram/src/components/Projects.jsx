979ef1f7f324ee4fd519f98f60d69fe97bf14bc3
Gangaram1998 <ramggawade1998@gmail.com>
2023-05-31 11:32:51 +0530

    add project

--- gangaram/src/components/Projects.jsx
+++ gangaram/src/components/Projects.jsx
@@ -3,18 +3,19 @@ import { Box,Text,Grid,GridItem,Img, Button } from '@chakra-ui/react'
 import AOS from "aos";
 
 import "aos/dist/aos.css";
-import ayoa from "./ayoa.png"
+// import ayoa from "./ayoa.png"cd g
+import health from "./helthdr.png"
 import cronofit from "./cronofit.png"
 import HotStar from "./HotStar.png"
-import BabyBliss from "./BabyBliss.png"
-import goodcluck from "./goodcluck.png"
+// import BabyBliss from "./BabyBliss.png"
+import fitfusion from "./fitfusion.png"
 const Projects = () => {
     AOS.init();
   return (
     <Grid id="Projects" m="auto" w="80%" mt={{base:"50",sm:"50",md:"70"}}> 
         <Text as="b" fontSize={"5xl" } >Projects</Text>
         {/* BabyBliss */}
-<GridItem display={{base:"grid",sm:"grid",md:"flex"}} justifyContent="space-between" mt={{base:"50",sm:"70",md:"100"}}>
+{/* <GridItem display={{base:"grid",sm:"grid",md:"flex"}} justifyContent="space-between" mt={{base:"50",sm:"70",md:"100"}}>
 <Box w={{base:"80%", sm:"90%",md:"40%"}} m="auto" data-aos="fade-up"  data-aos-delay="300">
 <Img src={BabyBliss}  justifyContent="space-between" />
 </Box>
@@ -47,22 +48,21 @@ const Projects = () => {
        }} bg="#22d3ee">Live</Button>
         </Box>
     </Box>
-</GridItem>
+</GridItem> */}
 
 <GridItem display={{base:"grid",sm:"grid",md:"flex"}} justifyContent="space-between" mt={{base:"50",sm:"70",md:"100"}}>
     <Box w={{base:"80%", sm:"90%",md:"40%"}} m="auto" data-aos="fade-up" data-aos-delay="300">
-        <Img src={ayoa}  />
+        <Img src={health}  />
     </Box>
     <Box w={{base:"80%", sm:"90%",md:"40%"}} m="auto" data-aos="fade-bottom"  data-aos-delay="600">
-        <Text as="b" fontSize={"3xl"} color="#319795">Ayoa.com Clone</Text>
+        <Text as="b" fontSize={"3xl"} color="#319795">Health Dr.</Text>
    
     <br></br>
-        <Text as="b">This project is about a website though which we can Plan and Learn with the help of AI 
-            and can schedule our tasks effectively.
+        <Text as="b">Health-Dr. website is used to track your health-diet. Here you can set the goal to have a fitness full body and our website will track your fitness
         </Text>
         
         <br></br>
-        <Text as="b" color={"#4FA095"}>A Solo project, developed in 4 days.</Text>
+        <Text as="b" color={"#4FA095"}>A Colloberative Project, devoloped by the team of 5 Developers in 4 days.</Text>
 <br></br>
 <br></br>
         <Box display="flex" justifyContent="space-between" w={{base:"80%",sm:"80%", md:"80%"}} m="auto">
@@ -76,15 +76,15 @@ const Projects = () => {
         </Box>
         <Box display={"flex"} justifyContent={{base:"space-between",sm:"space-around" ,md:"space-between"}} w={{base:"80%",sm:"90%", md:"60%"}} m="auto" mt="5">
        <Button onClick={()=>{
-        window.open("https://github.com/RationalPrabal/Ayoa-Clone","_blank")
+        window.open("https://github.com/Gangaram1998/Health-Dr.","_blank")
        }} bg="#86efac">Github</Button>
         <Button onClick={()=>{
-        window.open("https://frolicking-kelpie-5c21d5.netlify.app/","_blank")
+        window.open("https://incomparable-blancmange-56e496.netlify.app/","_blank")
        }} bg="#22d3ee" >Live</Button>
         </Box>
     </Box>
 </GridItem>
-<GridItem display={{base:"grid",sm:"grid",md:"flex"}} justifyContent="space-between" mt={{base:"50",sm:"70",md:"100"}}>
+{/* <GridItem display={{base:"grid",sm:"grid",md:"flex"}} justifyContent="space-between" mt={{base:"50",sm:"70",md:"100"}}>
 <Box w={{base:"80%", sm:"90%",md:"40%"}} m="auto" data-aos="fade-up" data-aos-delay="600">
 <Img src={cronofit} />
 </Box>
@@ -95,7 +95,7 @@ const Projects = () => {
         <Text as="b">This project is about a website through which we can Track our diet, exercise, and health data</Text>
         
         <br></br>
-        <Text as="b" color={"#4FA095"}>A Colloberative Project, devoloped by the team of 5 Developers in 4 days.Worked as Project Lead.</Text>
+        <Text as="b" color={"#4FA095"}>A Colloberative Project, devoloped by the team of 4 Developers in 2 days.</Text>
         <br></br>
 <br></br>
         <Box display="flex" justifyContent="space-between" w={{base:"80%",sm:"80%", md:"80%"}} m="auto">
@@ -116,36 +116,39 @@ const Projects = () => {
        }} bg="#22d3ee">Live</Button>
         </Box>
     </Box>
-</GridItem>
+</GridItem> */}
 <GridItem display={{base:"grid",sm:"grid",md:"flex"}} justifyContent="space-between" mt={{base:"50",sm:"70",md:"100"}}>
 <Box w={{base:"80%", sm:"90%",md:"40%"}} m="auto" data-aos="fade-up" data-aos-delay="300">
-<Img src={goodcluck}  justifyContent="space-between" />
+<Img src={fitfusion}  justifyContent="space-between" />
 </Box>
     <Box w={{base:"80%", sm:"90%",md:"40%"}} m="auto" data-aos="fade-bottom" data-aos-delay="300">
-    <Text as="b" fontSize={"3xl"} color="#319795">KFC-Clone</Text>
+    <Text as="b" fontSize={"3xl"} color="#319795">Fitfusion</Text>
     
     <br></br>
-        <Text as="b">This Project is about a website through which we can Order various Veg & Non-Veg Fast-Foods.</Text>
+        <Text as="b">This Project is about a website through which you can get fitness tips and fitness education.</Text>
         
         <br></br>
-        <Text as="b" color={"#4FA095"}>A Solo Project, developed in 4 days.</Text>
+        <Text as="b" color={"#4FA095"}>A Colloberative Project, devoloped by the team of 4 Developers in 2 days.</Text>
         <br></br>
 <br></br>
         <Box display="flex" justifyContent="space-between" w={{base:"80%",sm:"80%", md:"80%"}} m="auto">
         <Box ><Img  align="center"  src="https://img.shields.io/badge/React-5ccfee?style=for-the-badge&logo=react"  alt="react js" /></Box>
-            <Box >
+            {/* <Box >
             <Img align="center" src = "https://img.shields.io/badge/Chakra UI-2abfb3?style=for-the-badge&logo=chakra ui"  alt="chakra ui" />
+            </Box> */}
+            <Box >
+            <Img align="center" src ="https://img.shields.io/badge/styled Components-2962ff?style=for-the-badge&logo=styled"  alt="Rest API"/>
             </Box>
             <Box >
-            <Img align="center" src ="https://img.shields.io/badge/Rest API-2962ff?style=for-the-badge&logo=api"  alt="Rest API"/>
+            <Img align="center" src ="https://img.shields.io/badge/Typescript-e36028?style=for-the-badge&logo=typescript"  alt="TypeScript"/>
             </Box>
         </Box>
         <Box display={"flex"} justifyContent={{base:"space-between",sm:"space-around" ,md:"space-between"}} w={{base:"80%",sm:"90%", md:"60%"}} m="auto" mt="5">
         <Button onClick={()=>{
-        window.open("https://github.com/RationalPrabal/KFC-clone",'_blank')
+        window.open("https://github.com/Gangaram1998/fitfusion",'_blank')
        }} bg="#86efac">Github</Button>
         <Button onClick={()=>{
-        window.open("https://goodcluck.netlify.app/",'_blank')
+        window.open("https://fitfusion.vercel.app/",'_blank')
        }} bg="#22d3ee">Live</Button>
         </Box>
     </Box>
